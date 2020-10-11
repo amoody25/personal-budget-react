f6a22f12e55e62709edaa92db7948a1ac67f7891
Austin Moody <amoody25@uncc.edu>
2020-10-11 14:28:02 -0400

    Migrated Personal Budget app to React

---
+++ src/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      <footer>    
+        <div className="bottom">
+            <div className="center">
+                All rights reserved &copy; Austin Moody
+            </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div>
+      <header role="banner" aria-label="title block">
+          <div className="hero">
+              <h1>Personal Budget</h1>
+              <h2>A personal-budget management app</h2>
+          </div>
+      </header>
+    </div>
+  );
+}
+
+export default Hero;
